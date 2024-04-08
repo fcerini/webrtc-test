ad7b986f733e8df174657410fa30fcdf52366293
Fernando Cerini <fcerini@gmail.com>
2024-04-08 18:36:25 -0300

    api

---
+++ api.go
@@ -0,0 +1,49 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
+// Package signal contains helpers to exchange the SDP session
+// description between examples.
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+func ApiSDP(w http.ResponseWriter, r *http.Request) {
+	body, _ := io.ReadAll(r.Body)
+	w.Write([]byte(`{"Status": "OK"}`))
+	gloSdpChan <- string(body)
+
+}
+
+func ApiRestart(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`{"Status": "OK"}`))
+	log.Fatal("ApiRestart")
+}
+
+// Encode encodes the input in base64
+func Encode(obj interface{}) string {
+	b, err := json.Marshal(obj)
+	if err != nil {
+		panic(err)
+	}
+
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+// Decode decodes the input from base64
+func Decode(in string, obj interface{}) {
+	b, err := base64.StdEncoding.DecodeString(in)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(b, obj)
+	if err != nil {
+		panic(err)
+	}
+}
